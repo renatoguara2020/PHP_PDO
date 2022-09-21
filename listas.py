30e002a8e798640b54a7f5de987b9bf10a0aed3f
Renato Alves Soares <renatoguara2020@gmail.com>
2022-09-21 09:21:19 -0300

    ok

--- listas.py
+++ listas.py
@@ -1,3 +1,11 @@
 lista_compras = ['banana', 'laranja', 'maçã']
 print(lista_compras)
 ['banana', 'laranja', 'maçã']
+
+lista_compras.append('Kiwi verde')
+
+print(lista_compras)
+
+del lista_compras[1]
+
+print(lista_compras)
